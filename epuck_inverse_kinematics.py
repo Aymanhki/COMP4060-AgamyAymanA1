270956ddf06a6768e177641f98da63f8e4648160
vRuShIl13 <patelvk1@myumanitoba.ca>
2025-01-15 13:09:29 -0600

    task 4 - inverse kinematics sample code.

---
+++ epuck_inverse_kinematics.py
@@ -0,0 +1,49 @@
+import math
+import numpy as np
+import epuck_helper_functions as helper
+
+def diff_drive_inverse_kin(distance_mm, speed_mm_s, omega_rad):
+    """
+    :param distance_mm: distance to be travelled
+    :param speed_mm_s: signed speed, negative if moving backwards
+    :param omega_rad: angle of turn
+    :return: left wheel speed, right wheel speed (in steps), total_left_steps, total_right_steps
+
+    """
+    if distance_mm == 0:
+        # Angular velocity (rad/s) derived from speed and axle length
+        angular_velocity_rad = abs(speed_mm_s) / (helper.AXLE_LENGTH_MM / 2)
+
+        # Distances each wheel travels during the rotation
+        left_distance_mm = -omega_rad * helper.AXLE_LENGTH_MM / 2
+        right_distance_mm = omega_rad * helper.AXLE_LENGTH_MM / 2
+
+        # Wheel speeds (mm/s)
+        left_speed_mm = -angular_velocity_rad * (helper.AXLE_LENGTH_MM / 2)
+        right_speed_mm = angular_velocity_rad * (helper.AXLE_LENGTH_MM / 2)
+
+    else:
+        time_s = abs(distance_mm / speed_mm_s)  # Time to travel the given distance
+        angular_velocity_rad = omega_rad / time_s if time_s != 0 else 0
+
+        left_speed_mm = speed_mm_s - (angular_velocity_rad * helper.AXLE_LENGTH_MM / 2)
+        right_speed_mm = speed_mm_s + (angular_velocity_rad * helper.AXLE_LENGTH_MM / 2)
+
+        left_distance_mm = distance_mm - (omega_rad * helper.AXLE_LENGTH_MM / 2)
+        right_distance_mm = distance_mm + (omega_rad * helper.AXLE_LENGTH_MM / 2)
+
+    # Convert speeds to steps/s
+    left_speed_steps = helper.mm_to_steps(left_speed_mm)
+    right_speed_steps = helper.mm_to_steps(right_speed_mm)
+
+    # Convert distances to total steps
+    left_steps = abs(helper.mm_to_steps(left_distance_mm))
+    right_steps = abs(helper.mm_to_steps(right_distance_mm))
+
+    return left_speed_steps, right_speed_steps, left_steps, right_steps
+
+if __name__ == "__main__":
+    print(diff_drive_inverse_kin(130, 10, 0))
+    print(diff_drive_inverse_kin(130, -10, 0))
+    print(diff_drive_inverse_kin(0, 50, np.pi / 2))
+    print(diff_drive_inverse_kin(0, -50, np.pi / 2))
